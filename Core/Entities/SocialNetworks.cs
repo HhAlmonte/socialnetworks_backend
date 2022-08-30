1480f0ff28559c9eec78dd861fcf880eae6fb09b
Hector Almonte <hbalmontess272@gmail.com>
2022-08-30 19:24:43 -0300

    creation of businesslogic, core and wepabi

---
+++ Core/Entities/SocialNetworks.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Entities
+{
+    public class SocialNetworks
+    {
+        public SocialNetworks(int status)
+        {
+            Status = status;
+        }
+
+        public string Author = "Hector Almonte";
+        public string Created = "8/30/2022";
+        public string Contact = "Hbalmontess272@gmail.com";
+        public int Status { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"Author: {Author} \n" +
+                $"Created: {Created} \n" +
+                $"Contact: {Contact} \n" +
+                $"Status: {Status} \n";
+        }
+    }
+}
---
+++ WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("socialnetworksapi/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var result = HttpContext.Response.StatusCode;
+
+            SocialNetworks socialNetworks = new SocialNetworks(result);
+
+            return socialNetworks.ToString();
+
+            /*await HttpContext.Response.WriteAsync($"Status: {result}");*/
+        }
+    }
+}
